4ffad7aded73040420700d9eeed30df3a2cd3eb0
Prashant Saini <prashantsaini.code@gmail.com>
2023-07-26 12:14:28 +0530

    Footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,10 @@
+import Link from "next/link";
+
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between ">
+            <Link href='/' className="font-bold text-xl">Saian</Link>
+            <p>© All rights reserved</p>
+        </div>
     )
 }
